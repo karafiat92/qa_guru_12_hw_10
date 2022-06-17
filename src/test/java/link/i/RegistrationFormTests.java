9bf2c6aec04c8e3e960f618659a1d05f84e9ffbd
Екатерина Антонова <karafiat@mail.ru>
2022-06-17 12:27:02 +0700

    added steps

--- src/test/java/link/i/RegistrationFormTests.java
+++ src/test/java/link/i/RegistrationFormTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 public class RegistrationFormTests {
     RegistrationFormPage regForm = new RegistrationFormPage();
     Faker faker = new Faker();
@@ -26,49 +28,61 @@ public class RegistrationFormTests {
             gender = regForm.getRandomGender(),
             hobby = regForm.getRandomHobby(),
             month = regForm.getRandomMonth(),
-            state = regForm.getRandomState();
+            state = regForm.getRandomState(),
+            cityValue = "",
+            subject = "";
     Integer day = faker.number().numberBetween(1, 30),
             year = faker.number().numberBetween(1900, 2022);
 
     @BeforeAll
     static void setUp() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1500x1080";
-        Configuration.holdBrowserOpen = false;
+        step("Задание конфигураций", ()->{
+            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.browserSize = "1500x1080";
+            Configuration.holdBrowserOpen = false;
+        });
     }
 
     @Test
     @DisplayName("Регистрация с позитивными рандомными данными")
     void positiveValuesRegistrationForm() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        regForm.openPage();
-//        инициализация переменных, для последующей работы с тестом
-        String subject = regForm.getRandomSubject("o");
-        regForm.setFirstname(firstname)
-                .setLastname(lastname)
-                .setEmail(userEmail)
-                .setGender(gender)
-                .setPhoneNumber(phoneNumber)
-                .setBirthDate(day, month, year.toString())
-                .setSubject(subject)
-                .setHobby(hobby)
-                .setPhoto(pictureName)
-                .setAddress(currentAddress)
-                .setState(state);
-        String cityValue = regForm.setCity(state);
-        regForm.submitForm();
-//        Проверка, что все данные ушли верно
-        regForm.checkWindowExist()
-                .checkValueInTable("Student Name", (firstname + " " + lastname))
-                .checkValueInTable("Student Email", userEmail)
-                .checkValueInTable("Gender", gender)
-                .checkValueInTable("Mobile", phoneNumber.substring(0,10))
-                .checkValueInTable("Date of Birth", (day + " " + month + "," + year))
-                .checkValueInTable("Subjects", subject)
-                .checkValueInTable("Hobbies", hobby)
-                .checkValueInTable("Picture", pictureName)
-                .checkValueInTable("Address", currentAddress)
-                .checkValueInTable("State and City", (state + " " + cityValue))
-                .closeForm();
+        step("Открытие формы", () -> {
+            regForm.openPage();
+        });
+        step("Инициализация переменных, для последующей работы с тестом", () -> {
+            subject = regForm.getRandomSubject("o");
+            regForm.setFirstname(firstname)
+                    .setLastname(lastname)
+                    .setEmail(userEmail)
+                    .setGender(gender)
+                    .setPhoneNumber(phoneNumber)
+                    .setBirthDate(day, month, year.toString())
+                    .setSubject(subject)
+                    .setHobby(hobby)
+                    .setPhoto(pictureName)
+                    .setAddress(currentAddress)
+                    .setState(state);
+            cityValue = regForm.setCity(state);
+        });
+        step("Подтверждение введённых данных", () -> {
+            regForm.submitForm();
+        });
+        step("Проверка корректности отображения введённых данных",()-> {
+            regForm.checkWindowExist()
+                    .checkValueInTable("Student Name", (firstname + " " + lastname))
+                    .checkValueInTable("Student Email", userEmail)
+                    .checkValueInTable("Gender", gender)
+                    .checkValueInTable("Mobile", phoneNumber.substring(0,10))
+                    .checkValueInTable("Date of Birth", (day + " " + month + "," + year))
+                    .checkValueInTable("Subjects", subject)
+                    .checkValueInTable("Hobbies", hobby)
+                    .checkValueInTable("Picture", pictureName)
+                    .checkValueInTable("Address", currentAddress)
+                    .checkValueInTable("State and City", (state + " " + cityValue));
+        });
+       step("Закрытие формы",()-> {
+           regForm.closeForm();
+       });
     }
 }
