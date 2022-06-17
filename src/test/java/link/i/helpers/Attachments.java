b06f6ba9db59a97bae35521ffcf5d234ff2620e9
Екатерина Антонова <karafiat@mail.ru>
2022-06-17 15:00:27 +0700

    added video attachment

--- src/test/java/link/i/helpers/Attachments.java
+++ src/test/java/link/i/helpers/Attachments.java
@@ -15,23 +15,27 @@ import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attachments {
     @Attachment(value = "{attachName}", type = "text/plain")
-    public static String attachAsText(String attachName, String message){
+    public static String attachAsText(String attachName, String message) {
         return message;
     }
+
     @Attachment(value = "Page source", type = "text/plain")
     public static byte[] pageSource() {
         return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
     }
+
     @Attachment(value = "{attachName}", type = "image/png")
-    public static byte[]  screenshotAs(String attachName){
+    public static byte[] screenshotAs(String attachName) {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
+
     public static void browserConsoleLogs() {
         attachAsText(
-          "Browser console logs",
-          String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+                "Browser console logs",
+                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
         );
     }
+
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
